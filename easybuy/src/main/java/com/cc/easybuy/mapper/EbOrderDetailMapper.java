0e1a706264245f99d651a557928e2c150374675b
coltzx <coltchn@gmail.com>
2021-06-26 07:53:39 +0800

    增加了EbOrderDetailMapper

---
+++ easybuy/src/main/java/com/cc/easybuy/mapper/EbOrderDetailMapper.java
@@ -0,0 +1,9 @@
+package com.cc.easybuy.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cc.easybuy.model.EbOrderDetail;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface EbOrderDetailMapper extends BaseMapper<EbOrderDetail> {
+}
